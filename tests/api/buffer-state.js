9acb6442d60e2b8cb2b8bfd990d4ca3426618b20
jlxip <jlxip@jlxip.net>
2026-09-09 12:54:12 +0200

    test: reproduce async buffer state restore failure

---
+++ tests/api/buffer-state.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import assert from "node:assert/strict";
+import { SyncBuffer, buffer_from_object } from "../../src/buffer.js";
+
+const size = 1024;
+const offset = 256;
+const contents = new Uint8Array(256).fill(0x5A);
+
+const remote = buffer_from_object({ url: "disk.img", size });
+remote.set(offset, contents, () => {});
+
+const local = new SyncBuffer(new ArrayBuffer(size));
+local.set_state(remote.get_state());
+
+assert.deepEqual(local.get_from_cache(offset, contents.length), contents);
